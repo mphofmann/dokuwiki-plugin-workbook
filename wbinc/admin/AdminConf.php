f47946c6b29840368949b0b7bdcdaaeaec2bb3d5
michael@mphofmann.com <c3qdeWKxKGUw>
2021-05-10 19:20:07 +0200

    x

---
+++ wbinc/admin/AdminConf.php
@@ -0,0 +1,29 @@
+<?php
+namespace workbook\wbinc\admin;
+class AdminConf {
+    /* -------------------------------------------------------------------- */
+    private static $__Ar = [];
+    private static $__DefaultAr = ['plugin' => ['workbook' => ['connect_dist' => 'stable', 'connect_version' => '2020-07-29']]];
+    /* -------------------------------------------------------------------- */
+    public static function Get($inVar1, $inVar2 = '', $inVar3 = ''): string {
+        if (empty(self::$__Ar)) {
+            $conf = [];
+            $ar = [WB_DATACONF . 'local.php', WB_DATACONF . 'local.protected.php'];
+            foreach ($ar as $file) {
+                if (file_exists($file)) {
+                    include($file); // TODO ?
+                }
+            }
+            self::$__Ar = $conf;
+        }
+        if (empty($inVar2)) {
+            $return = self::$__Ar[$inVar1] ?? self::$__DefaultAr[$inVar1];
+        } elseif (empty($inVar3)) {
+            $return = self::$__Ar[$inVar1][$inVar2] ?? self::$__DefaultAr[$inVar1][$inVar2];
+        } else {
+            $return = self::$__Ar[$inVar1][$inVar2][$inVar3] ?? self::$__DefaultAr[$inVar1][$inVar2][$inVar3];
+        }
+        return $return ?? '';
+    }
+    /* -------------------------------------------------------------------- */
+}
